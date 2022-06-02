268d5b29c110f98b508c0f4fa9d5c5bece329df5
Eric <tryharder3l33t@gmail.com>
2022-06-02 17:46:52 -0400

    heroku deploy

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>client/src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://github.com/TryHarder3l33t/capstone'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Go to our github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//configure port in package.json
+const PORT = process.env.PORT;
+
+//needed for heroku deploy
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World the server is running :(');
+});
+
+//React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
