c1369df5148f2b91760a839563aa9fc0a392c1bd
Gary Smith <garysmith193358@gmail.com>
2022-06-11 12:27:34 -0400

    triangle instance setup, can now revolve around circle

--- client/src/Visualizer.js
+++ client/src/Visualizer.js
@@ -7,6 +7,10 @@ import 'p5/lib/addons/p5.sound';
 let currentSound;
 let fft;
 let angle = 0
+   
+     // Circle's radius
+     const radius = 200;
+const numOfTriangles = new Array(1)
 
 // react-p5 has this so we can use p5 methods outside of draw, and set up.
   const myp5 = new window.p5()
@@ -17,7 +21,7 @@ let angle = 0
 const Visualizer = () => {
   const [audio, setAudio] = useState('')
   
-  //function that is passed to the sketch component as a prop
+  // function that is passed to the sketch component as a prop
   const setup = (p, canvasParentRef) => {
     p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
     fft = new P5.FFT()
@@ -25,39 +29,12 @@ const Visualizer = () => {
   
     //function that is passed to the sketch component as a prop
   const draw = (p) => {
+    //sets the background color
     p.background("#243A73")
-
-    const pieces = 50;
-    
-     // Circle's radius
-  const radius = 200;
 
   // Move the origin to the center of the canvas
   p.translate( p.width/2, p.height/2 );
 
-  // The centered circle
-  p.stroke('#37E2D5');
-  p.ellipse( 0, 0, radius );
-
-  //may keep this commented out for now -GS
-
-  // For each piece draw a line
-  // for( let i = 0; i < pieces; i++ ) {
-  //   // Rotate the point of origin
-  //   p.rotate( p.TWO_PI / pieces );
-    
-  //   // Draw the red lines
-  //       p.push()
-  //   p.stroke('white');
-  //   p.rotate(angle)
-  //   p.line( 10, radius/2, 0, 140 );
-
-  //   // p.stroke( 0 );
-  //   // p.line( -10, radius/2, 0, 140 ); 
-  //   p.pop()
-  // }
-  angle -= p.radians(0.3)
-  
     // This is what catches the pitches
   fft.analyze();
 
@@ -71,39 +48,76 @@ const Visualizer = () => {
   const mapMid      = p.map( mid, 0, 255, -150, 150 );
   const mapTreble   = p.map( treble, 0, 255, -200, 200 );
 
-  
-  for( let i = 0; i < pieces; i++ ) {
-    p.rotate( p.TWO_PI / pieces );
-    
-    //sets color for bass line - This is currently yellow.
-    p.stroke("#FBCB0A");
-    // Draw the bass lines
-    p.line( mapBass, radius/2, 0, radius )
-  
-   
+//sets the circle ring
     p.push()
-    //sets the color for mid lines - This is currently blue
-    p.stroke('#37E2D5');
-    p.rotate(angle)
-     // Draw the mid lines
-    p.line( mapMid, 300, 0, radius );
-    p.line( -mapMid, 300, 0, radius );   
-    p.pop()
+  p.stroke(255)
+  p.noFill()
+  p.strokeWeight(3)
+  p.circle(0,0,radius*2)
+  p.pop()
 
-    // angle += p.radians(0.3)
-
-    //sets the color for treble lines - This is currently yellow
-    p.stroke("#C70A80");
-    // Draw the treble lines
-    p.line( mapTreble, 1, 0, 140 );        
-    
+  class Triangle {
+    constructor(x,y) {
+      this.pos = p.createVector(x,y)
+      this.vel = p.createVector(1,0)
+      this.acc = p.createVector(0,0)
+      this.angle = angle;
+      this.radius = 200;
     }
+
+    draw() {
+      p.strokeWeight(10)
+      p.stroke("red")
+      //moves point of origin
+      p.translate(this.pos.x, this.pos.y)
+
+      //this is what makes the triangle point in the direction it is currently going - currently does not work
+      this.angle = this.vel.heading()
+      p.rotate(this.angle)
+     p.triangle(-1, -1, -1, 1, 1, 0)
+    }
+
+    
+    //WATCH THE VECTOR VIDEO
+   update() {
+  
+    // const p5 = Object.getPrototypeOf(p).constructor
+    // this.angle += p.radians(0.03)
+    // // p.rotate(this.angle)
+    // this.vel = p5.Vector.fromAngle(this.angle)
+
+    // this.pos.add(this.vel)
+    }
+  
   }
+
+
+  for (let i = 0; i < numOfTriangles.length; i++) {
+    let x = radius * p.cos(angle)
+    let y = radius * p.sin(angle)
+    
+      numOfTriangles[i] = new Triangle(x,y, angle)
+  }
+
+
+    // create triangles
+    p.stroke('red')
+    p.strokeWeight(10)
+
+    for (let i = 0; i < numOfTriangles.length; i++) {
+      // numOfTriangles[i].update()
+      numOfTriangles[i].draw()
+      angle+= p.radians(1)
+    }
+    
+  }
+
+
+
 
   //sets the currentSound to the audio
   const preload = () => {
     currentSound = myp5.loadSound(audio)
-    console.log(currentSound)
   }
   
     //any time audio is changed, run preload
@@ -124,12 +138,16 @@ const Visualizer = () => {
           currentSound.play()  
        }
   }    
+
+  const windowResized = () => {
+    myp5.resizeCanvas(myp5.windowWidth, myp5.windowHeight);
+  }
   
   
   return (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <div>
-        <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} />
+        <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} windowResized={windowResized}/>
       </div>
       
       <input type="file" name="file" accept="audio/*" onChange={(event) => setAudio(event.target.files[0])}/>
