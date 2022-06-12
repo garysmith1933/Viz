c7a8802e5099995c0d6950fbccdefdab2d24dd44
Eric <tryharder3l33t@gmail.com>
2022-06-12 12:53:01 -0400

    postgresql readme updated

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const seed = require('./db/seed');
 const app = require('./Server');
 
 //you can configure port in package.json for some reason windows not seeing port in package.json script
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+
+seed();
