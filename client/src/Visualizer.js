dc933125f167e6589db6ca53bca0a6261ddba135
Gary Smith <garysmith193358@gmail.com>
2022-06-14 01:41:46 -0400

    added ability to increase, and decrease speed, added notes on how everything works, fixed windowResized, and reorganized page

--- client/src/Visualizer.js
+++ client/src/Visualizer.js
@@ -2,20 +2,41 @@ import React, { useState, useEffect } from 'react'
 import Sketch from 'react-p5';
 import 'p5/lib/addons/p5.sound';
 
-//I was playing around with the look of the visualizer. Feel free to mess around with this yourselves. 
+//IMPORTANT: Each array has a length of 5, the order is treble, lowmid, mid, highmid, bass
+
+//I have some notes on it later down the code but ill put some instructions here as well.
+
+// W key = Reverse direction of where the diamonds are currently going
+//A key = increases the top speed of diamonds when a song is playing by a set amount
+//S key = decreases the top speed of diamonds when a song is playing by a set amount
+// Enter key = Cycles through colors! 
+
+// react-p5 has this so we can use p5 methods outside of draw, and set up.
+const myp5 = new window.p5()
+//to get access to the fft function in setup, I needed to get the prototype of this instance of P5 constructor, dont ask me why.
+const P5 = Object.getPrototypeOf(myp5).constructor
 
 let currentSound;
 let fft;
+//if you try to make this one variable, they will all move in unison, we dont want that.
 let angles = [45, 45, 45, 45, 45]
 let selectedPalette = 0
-let swap
-let windowResized
+let selectedSpeed = 2
+  
+// Circle's radius
+let radius = 100;
+//stretches out the placements of the circles
+const radiusMultiplier = [1, 1.5, 2,2.5,3]
+const numOfCircles = 5
+//if its 1, it will go right, -1 it will go left.
+const directions = [-1, 1, -1, 1, -1]
+let windowWidth = myp5.windowWidth
+let windowheight = myp5.windowHeight
 
-//An array of colors to pick from. - Will set up later colors[0][0]
-const colors = 
-[
-//Considering setting the ones thats not the base to be color templates based on the tempo of the song, ex: engergetic, slow.. Not sure. -GS
 
+//color palette 
+const colors = [
+//Considering setting the ones thats not the base to be color templates based on the tempo of the song, ex: engergetic, slow.. Not sure. Feel free to play around with them -GS
   //Base - I ll fight over this one
   ['#ffbe0b','#fb5607','#ff006e','#8338ec','#3a86ff'],
 
@@ -30,39 +51,31 @@ const colors =
 
   //Cotton Candy
   ["#cdb4db","#ffc8dd",'#ffafcc', '#bde0fe', '#a2d2ff']
-
 ]
 
-   
-// Circle's radius
-let radius = 100;
-const radiusMultiplier = [1, 1.5, 2,2.5,3]
-const numOfCircles = 5
-const anglePos = [0,0,0,0,0]
-const directions = [-1, 1, -1, 1, -1]
+const topSpeeds = [
+  //slowest
+  [1.4, 0.6, 0.8, 1, 0.6],
+  //slower
+  [2, 0.8, 1.4, 1.4, 0.8],
+  //normal - this is the default speed before the cycle
+  [2.6, 1.0, 2, 1.8, 1.0],
+  //faster
+  [3.2, 1.2, 2.6, 2.4, 1.2],
+  //fastest
+  [3.8, 1.4, 3.2, 2.8, 1.4]
+]
 
-// react-p5 has this so we can use p5 methods outside of draw, and set up.
-  const myp5 = new window.p5()
-   //to get access to the fft function in setup, I needed to get the prototype of this instance of P5 constructor, dont ask me why.
-  const P5 = Object.getPrototypeOf(myp5).constructor
  
-
 const Visualizer = () => {
   const [audio, setAudio] = useState('')
   
   // function that is passed to the sketch component as a prop
   const setup = (p, canvasParentRef) => {
-    p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
+    p.createCanvas(windowWidth, windowheight).parent(canvasParentRef)
     fft = new P5.FFT()
     p.frameRate(120)
-
-    //still not working
-    windowResized = () => {
-      p.resizeCanvas(p.windowWidth, p.windowHeight);
-    }
-  
   }
-
 
     //function that is passed to the sketch component as a prop
   const draw = (p) => {
@@ -76,43 +89,35 @@ const Visualizer = () => {
   fft.analyze();
 
   // Get the volumes of different frequency ranges
-  const bass    = fft.getEnergy("bass");
-  const mid     = fft.getEnergy("mid");     
   const treble  = fft.getEnergy("treble");
   const lowMid = fft.getEnergy("lowMid")
+  const mid     = fft.getEnergy("mid"); 
   const highMid = fft.getEnergy("highMid")
-
-  //for rotation speed of diamonds - NEED TO TWEEK THIS
-  const trebleSpeed = p.map(treble, 0, 255, 0.3, 5)
+  const bass    = fft.getEnergy("bass");
+  
+  //for rotation speed of diamonds
+  const trebleSpeed = p.map(treble, 0, 255, 0.3, topSpeeds[selectedSpeed][0])
   //second slowest
-  const lowMidSpeed = p.map(lowMid, 0, 255, 0.3, 1.2)
-  const midSpeed = p.map(mid, 0, 255, 0.3, 2.5)
-  const highMidSpeed = p.map(highMid, 0, 255, 0.3, 1.5)
+  const lowMidSpeed = p.map(lowMid, 0, 255, 0.3, topSpeeds[selectedSpeed][1])
+  const midSpeed = p.map(mid, 0, 255, 0.3, topSpeeds[selectedSpeed][2])
+  const highMidSpeed = p.map(highMid, 0, 255, 0.3, topSpeeds[selectedSpeed][3])
   //slowest
-  const bassSpeed = p.map(bass, 0, 255, 0.3, 1.2)
+  const bassSpeed = p.map(bass, 0, 255, 0.3, topSpeeds[selectedSpeed][4])
   
-
+  //array of speeds
   const speeds = [trebleSpeed, lowMidSpeed, midSpeed, highMidSpeed, bassSpeed]
-
-
-  //Whats left?
-  //draw watermarks? trail
-  // fix colors
-  // preset speed adjustments 
   
-
-  // sets the size to the diamonds that are in the treble circle based on volume of treble 
+  //takes the volume of the frequencies and sets them the size of a variable 
   const sizeTreble = p.map(treble, 0, 255, 15, 45)
-  //second heaviest
   const sizeLowMid = p.map(lowMid , 0 , 255, 15, 60)
   const sizeMid = p.map(mid, 0, 255, 15, 30)
   const sizeHighMid = p.map(highMid , 0 , 255, 15, 45)
-  //heaviest
   const sizeBass = p.map(bass, 0, 255, 15, 60)
 
+  //then we shove it in this array
   const sizes = [sizeTreble, sizeLowMid, sizeMid, sizeHighMid, sizeBass]
 
-
+  //each diamond has a position based on x and y coordinates, and angle they are placed set at, and a size, color and speed that can vary.
     class Diamond {
       constructor(x, y, a, size, color, speed) {
         this.pos = p.createVector(x,y)
@@ -122,7 +127,6 @@ const Visualizer = () => {
         this.speed = speed
       }
   
-      //Draws the diamonds
       draw() {
         p.push()
         //sets angle placed on circle, prevents other diamonds from being on top of each other
@@ -135,7 +139,7 @@ const Visualizer = () => {
           p.stroke(0)
           //gives the black outline
           p.strokeWeight(1)
-          //draws the diamond shape depending on the size passed when new instance is created.
+          //draws the diamond shape depending on the size passed when new instance was created.
           p.beginShape();
             p.vertex(0, this.size);
             p.vertex(this.size, 0);
@@ -146,25 +150,20 @@ const Visualizer = () => {
       }
     }
 
-
-  //sets the circle rings 
+  //sets the circle rings..that you cant see!
   for (let j = 1; j <= numOfCircles; j++){
     let current = j-1
-    p.push()
-    p.stroke(0)
-    p.noFill()
-    p.strokeWeight(2)
-    p.circle(0,0,radius*(j+1))
-    p.pop()
+  //once upon a time there was circles being drawn here
 
-  //polar to cartesian coordinates, basically..this is where our diamond drawing will begin
-    const x = (radius * radiusMultiplier[j-1]) * p.cos(angles[j-1]+anglePos[j-1])
-    const y = (radius * radiusMultiplier[j-1]) * p.sin(angles[j-1]+anglePos[j-1])
+  //polar to cartesian coordinates, basically..this is where our diamond drawing will begin and how it moves around!
+    const x = (radius * radiusMultiplier[current]) * p.cos(angles[current])
+    const y = (radius * radiusMultiplier[current]) * p.sin(angles[current])
 
     //for every 2 degrees moved place a diamond 
     for (let a = 0; a < p.radians(12); a+=p.radians(2)) {
       //Makes diamonds instances 
       const diamond = new Diamond(x,y,a, sizes[current], p.color(colors[selectedPalette][current]), speeds[current], directions[current])
+      //this is what draws the diamonds
       diamond.draw(a)
     }
 
@@ -172,6 +171,7 @@ const Visualizer = () => {
   angles[j-1] += directions[current] === 1 ? p.radians(speeds[current]): p.radians(-speeds[current])
   }
 }
+
   //sets the currentSound to the audio
   const preload = () => {
     currentSound = myp5.loadSound(audio)
@@ -185,23 +185,23 @@ const Visualizer = () => {
         currentSound.stop()
       }
     }
-    
     preload()
   },[audio])
 
-     
-     swap = () => {
-      // if(myp5.keyCode === 65) {
-      //   for (let i = 0; i < speeds.length; i++) {
-      //     speeds[i] = speeds[i] *= 1.5
-      //   }
-      // }
+    const keyPresses = () => {
+      // adds speed, press a 
+      if(myp5.keyCode === 65) {
+        //if you are at the max speed, chill out!
+        if(selectedSpeed === 4) return;
+        selectedSpeed++
+      }
 
-      // if(myp5.keyCode === 65) {
-      //   for (let i = 0; i < speeds.length; i++) {
-      //     speeds[i] = speeds[i] -= 1.5
-      //   }
-      // }
+      //decreases speed, press s
+      if(myp5.keyCode === 83) {
+        //if you are at the bottom and its still too much...I suck at adjusting speed lol
+        if(selectedSpeed === 0) return;
+        selectedSpeed--
+      }
 
     //Reverses direction on W key, at the moment
        if(myp5.keyCode === 87) {
@@ -216,15 +216,19 @@ const Visualizer = () => {
         if(selectedPalette === 4)  {
           //reset the cycle
           selectedPalette = 0;
-          return
+          return;
         }          
           selectedPalette++
       }
-      return false; // prevent default
+      return false; 
+    }
+
+    //if the window was set one way, this will readjust the drawing when it is changed.
+   const windowResized = () => {
+    myp5.resizeCanvas(myp5.windowWidth, myp5.windowHeight);
   }
 
-  
-  //in chrome, the pause feature will work once and then go back to the first time you paused it. However, this works as it should in Mozilla Firefox
+  //Pause / Play feature
   const mouseClicked = () => {
      if (currentSound.isPlaying()) {
        if(currentSound) {
@@ -235,16 +239,17 @@ const Visualizer = () => {
           currentSound.play()  
        }
   }    
-  
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center',}}>
       <div>
-        <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} windowResized={windowResized} keyPressed={swap}/>
+        <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} windowResized={windowResized} keyPressed={keyPresses}/>
       </div>
-      
-      <input type="file" name="file" accept="audio/*" onChange={(event) => setAudio(event.target.files[0])}/>
-      <button> Play </button>
-      {audio ? 'there is audio' : 'we no have that'}
+
+      <div style={{display: 'flex', justifyContent:'space-around'}}>
+        <input type="file" name="file" accept="audio/*" onChange={(event) => setAudio(event.target.files[0])}/>
+        <button> Play </button>
+      </div>
     </div>
     ) 
 }
