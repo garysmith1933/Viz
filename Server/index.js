400f210812d9bcdec234ac76d4f6a9b12b4b1552
Gary Smith <garysmith193358@gmail.com>
2022-06-25 11:48:53 -0400

    removed semicolon

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 //React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 });
 
 module.exports = app;
