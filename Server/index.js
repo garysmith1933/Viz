83e192e168e437d88cf71ba2146aa350f88eb662
Gary Smith <garysmith193358@gmail.com>
2022-06-25 10:51:50 -0400

    put back hello world

--- Server/index.js
+++ Server/index.js
@@ -15,12 +15,12 @@ app.use(express.static(__dirname + '/public'));
 //middleware stating if /api comes in send it to the folder api it will hit the index file which will route it
 app.use('/api', require('./api'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World the server is running :(');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello World the server is running :(');
+});
 
 //React
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
